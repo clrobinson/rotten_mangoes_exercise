40a932b933c2699bbe89982829e743107a379364
Chris Robinson <clrobinson@users.noreply.github.com>
2015-06-23 20:21:00 +0000

    Step 13 done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
   root to: 'movies#index'
 
-  resources :movies
+  resources :movies do 
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
